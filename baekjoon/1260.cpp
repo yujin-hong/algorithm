1cdb338f02d47ce6218a9f5028a1b1627e719508
yujin-hong <dbwlsdl2500@gist.ac.kr>
2021-03-01 13:40:05 +0900

    #baekjoon 1260

--- baekjoon/1260.cpp
+++ baekjoon/1260.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <string.h>
 
 using namespace std;
 
+vector<int> v[1001];
+int visited[1002] = {0,};
+
+void dfs(int start_node);
+void bfs(int start_node);
+
 int main(void) {
-    vector<int> v[1001];
+    freopen("input.txt","r",stdin);
 
     int node_num, line_num, start_node;
     cin>>node_num>>line_num>>start_node;
@@ -15,17 +23,41 @@ int main(void) {
         v[s].push_back(e);
         v[e].push_back(s);
     }
-
-    bfs(start_node);
+    for(int i=1;i<=node_num;i++) {
+        sort(v[i].begin(), v[i].end());
+    }
     dfs(start_node);
+    cout<<'\n';
+    memset(visited, 0, sizeof(visited));
+    bfs(start_node);
 }
 
 void dfs(int start_node) {
-    int visited[4];
-    cout<<start_node<<'\n';
-
+    visited[start_node] = 1;
+    cout<<start_node<<' ';
+    vector<int> connect_nodes = v[start_node];
+    for(int i=0;i<connect_nodes.size();i++) {
+        if(visited[connect_nodes[i]]==0) {
+            visited[connect_nodes[i]] = 1;
+            dfs(connect_nodes[i]);
+        }
+    }
 }
 
 void bfs(int start_node) {
-
+    queue<int> q;
+    visited[start_node] = 1;
+    q.push(start_node);
+    while(!q.empty()) {
+        int front = q.front();
+        cout<<front<<' ';
+        q.pop();
+        vector<int> connect_nodes = v[front];
+        for(int i=0;i<connect_nodes.size();i++) {
+            if(visited[connect_nodes[i]]==0) {
+                q.push(connect_nodes[i]);
+                visited[connect_nodes[i]] = 1;
+            }
+        }
+    }
 }

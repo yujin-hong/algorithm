55b56c695591eda65bda64adef60c56941029016
yujin-hong <dbwlsdl2500@gist.ac.kr>
2021-09-13 19:06:52 +0900

    test

--- baekjoon/10816.cpp
+++ baekjoon/10816.cpp
@@ -1,8 +1,27 @@
-7
-pop
-top
-push 123
-top
-pop
-top
-pop
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map<int, int> m;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("input.txt","r",stdin);
+    int num;
+    cin>>num;
+    for(int i=0;i<num;i++) {
+        int tmp;
+        cin>>tmp;
+        m[tmp]++;
+    }
+    int tc_num;
+    cin>>tc_num;
+    for(int i=0;i<tc_num;i++) {
+        int tmp;
+        cin>>tmp;
+        cout<<m[tmp]<<' ';
+    }
+    cout<<'\n';
+}

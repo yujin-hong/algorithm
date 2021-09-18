02ff216a2c0654f6ee2c2d38d2738b6894c670f0
yujin-hong <dbwlsdl2500@gist.ac.kr>
2021-09-18 11:11:37 +0900

    1676

---
+++ baekjoon/1676.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    // freopen("input.txt","r",stdin);
+    int num;
+    cin>>num;
+    cout<<num/5+num/25+num/125<<'\n';
+}
